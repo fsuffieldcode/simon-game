a1b4ebc7bc6f52b5df8da18b5bec061dd71ea9bd
fsuffieldcode <fsuffieldcode@gmail.com>
2019-09-28 14:45:08 +0100

    add start game function

--- game.js
+++ game.js
@@ -1,8 +1,30 @@
+// VARIABLES
+
+var userClickedPattern = [];
+
 var gamePattern = [];
 
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var userChosenColour = "";
+
+var level = 0;
+
+// LISTENERS
+
+$(".btn").on( "click", function() {
+    userChosenColour = this.id;
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+});
+
+$("html").one("keypress", function() {
+    $("h1").text("Level 0");
+    nextSequence();
+});
+
+// FUNCTIONS
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * Math.floor(4));
@@ -10,22 +32,22 @@ function nextSequence() {
     gamePattern.push(randomChosenColour);
     var selectedButton = $("#" + randomChosenColour);
     selectedButton.fadeOut(100).fadeIn(100);
-    var selectedAudio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    playSound(selectedButton);
+    level += 1;
+    $("h1").text("Level " + level);
+}
+
+
+function playSound(name) {
+    var selectedAudio = new Audio("sounds/" + name + ".mp3");
     selectedAudio.play();
 }
 
-$(".btn").on( "click", function(clicked) {
-    userChosenColour = this.id;
-});
 
-// $("#red").on( "click", function(clicked) {
-//     userChosenColour = this.id;
-// });
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function() {
+        $("#" + currentColour).removeClass("pressed")
+    }, 100);
+}
 
-// $("#yellow").on( "click", function(clicked) {
-//     userChosenColour = this.id;
-// });
-
-// $("#blue").on( "click", function(clicked) {
-//     userChosenColour = this.id;
-// });
